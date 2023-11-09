7d3853fdc5bee1ded1ba4ab5f113329cd93c63fa
Jason Hand <jason.hand@datadoghq.com>
2023-11-09 13:32:10 -0700

    Image Counter Added

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ let imagesDisplayed = 0; // Counter for the number of images displayed
 const maxImagesToShow = 6; // The maximum number of images to show before stopping
 const startButton = document.getElementById('startButton');
 const slideshowImage = document.getElementById('slideshowImage');
+// Add this line after you get your other elements by ID
+const imageCounter = document.getElementById('imageCounter');
 
 function preloadImages() {
   fetch('images.json')
@@ -31,11 +33,6 @@ function startSlideshow() {
   }
 
   function setNextImage() {
-    if (images.length === 0) {
-      console.log('No images to display.');
-      return;
-    }
-    
     if (imagesDisplayed >= maxImagesToShow) {
       stopSlideshow();
       return; // Stop showing more images
@@ -46,15 +43,17 @@ function startSlideshow() {
     
     // Retrieve the image at the random index
     const nextImage = images[currentIndex].replace(/&quot;/g, '').replace(/"/g, "");
-    const imgElement = document.getElementById('slideshowImage');
     
     // Set the image source
-    imgElement.style.backgroundImage = 'url(' + nextImage + ')';
+    slideshowImage.style.backgroundImage = 'url(' + nextImage + ')';
     
     // Apply the Ken Burns effect by adding the class to the element
-    imgElement.classList.remove('ken-burns-effect'); // Remove the class if it's already there
-    void imgElement.offsetWidth; // Trigger a reflow in between class changes
-    imgElement.classList.add('ken-burns-effect'); // Re-add the class to restart the animation
+    slideshowImage.classList.remove('ken-burns-effect'); // Remove the class if it's already there
+    void slideshowImage.offsetWidth; // Trigger a reflow in between class changes
+    slideshowImage.classList.add('ken-burns-effect'); // Re-add the class to restart the animation
+    
+    // Update the overlay text with the current image count
+    imageCounter.textContent = `${imagesDisplayed + 1} of ${maxImagesToShow}`;
     
     imagesDisplayed++; // Increment the counter
   }
@@ -72,10 +71,20 @@ function startSlideshow() {
 
 function stopSlideshow() {
   clearInterval(slideshowInterval);
-  currentIndex = 0;
-  startButton.style.display = 'block';
   slideshowImage.style.backgroundImage = '';
   slideshowImage.classList.remove('ken-burns-effect');
+  startButton.style.display = 'block';
+
+  // Check if the max images have been displayed
+  if (imagesDisplayed >= maxImagesToShow) {
+    // Wait for a short period before refreshing to allow the user to see the last image
+    setTimeout(function() {
+      location.reload();
+    }, 2000); // Wait for 2 seconds
+  } else {
+    // Reset the counter if the slideshow was stopped manually before the end
+    imagesDisplayed = 0;
+  }
 }
 
 startButton.addEventListener('click', startSlideshow);
@@ -87,3 +96,4 @@ document.addEventListener('keydown', (event) => {
 });
 
 window.onload = preloadImages;
+
